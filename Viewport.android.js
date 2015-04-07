d439e735a2270090138cde7b64239f17537e2bcf
Peter Janak <pjanak@nhl.com>
2015-04-07 17:46:56 -0400

    adding files

---
+++ Viewport.android.js
@@ -0,0 +1,17 @@
+/**
+ * Stub of Sample for Android.
+ *
+ * @providesModule Viewport
+ * @flow
+ */
+'use strict';
+
+var warning = require('warning');
+
+var Viewport = {
+    test: function() {
+	warning("Not yet implemented for Android.")
+    }
+};
+
+module.exports = Sample;
---
+++ Viewport.m
@@ -0,0 +1,74 @@
+#import "Viewport.h"
+#import "RCTBridge.h"
+#import "RCTEventDispatcher.h"
+#import "RCTUtils.h"
+
+static NSDictionary *RCTCurrentDimensions()
+{
+  static NSDictionary *dimensions;
+  
+  CGSize frameSize = [UIScreen mainScreen].applicationFrame.size;
+  if ((NSFoundationVersionNumber <= NSFoundationVersionNumber_iOS_7_1)
+      && UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)) {
+    frameSize = CGSizeMake(frameSize.height, frameSize.width);
+  }
+  
+  dimensions = @{
+                 @"width": @(frameSize.width),
+                 @"height": @(frameSize.height)
+                 };
+  
+  return dimensions;
+}
+
+
+@implementation Viewport
+{
+  NSDictionary *_lastKnownDimensions;
+}
+
+@synthesize bridge = _bridge;
+
+#pragma mark - Lifecycle
+
+- (instancetype)init
+{
+  if ((self = [super init])) {
+    _lastKnownDimensions = RCTCurrentDimensions();
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(deviceOrientationDidChangeNotification:)
+                                                 name:UIDeviceOrientationDidChangeNotification
+                                               object:nil];
+  }
+  
+  return self;
+}
+
+- (void)dealloc
+{
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark - Notification methods
+
+- (void)deviceOrientationDidChangeNotification:(NSNotification*)note
+{
+  _lastKnownDimensions = RCTCurrentDimensions();
+  [_bridge.eventDispatcher sendDeviceEventWithName:@"dimensionsDidChange" body:_lastKnownDimensions];
+}
+
+#pragma mark - Public API
+/**
+ * Get the current dimensions of the viewport
+ */
+- (void)getCurrentDimensions:(RCTResponseSenderBlock)callback
+{
+  RCT_EXPORT();
+  _lastKnownDimensions = RCTCurrentDimensions();
+  
+  callback(@[_lastKnownDimensions]);
+}
+
+
+@end
